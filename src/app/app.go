3b64f239ba64ae942b76f6181370b33644f1e5bc
Daniel Schott <da.schotti@gmail.com>
2016-04-09 10:44:12 +0200

    added recursive version

--- src/app/app.go
+++ src/app/app.go
@@ -4,32 +4,23 @@ import (
 	"fmt"
 	"strconv"
 	"filereader"
+	"knapsack"
 )
 
-type Item struct {
-	name   string
-	weight int
-	value  int
-}
 
-type Solution struct {
-	items       *[]Item // we just need the reference to save memory
-	totalValue  int
-	totalWeight int
-}
 
 func main() {
 
 	fmt.Println()
 	fmt.Println("---------------------------------------------------------------------------")
 	fmt.Printf("Wir arbeiten mit Google Go Go - https://www.youtube.com/watch?v=pIgZ7gMze7A \n")
+	fmt.Printf("Beispiel: Knapsack Problem (https://en.wikipedia.org/wiki/Knapsack_problem) \n")
 	fmt.Println("---------------------------------------------------------------------------")
 
 
 	// Read Knapsack Capacity from file
 	var capacity int
 	capacity = filereader.ReadCapacity("testdata/capacity")
-	fmt.Printf("Capacity: %[1]d \n", capacity)
 
 	// Read Weight of each item from File
 	var weight []int
@@ -40,18 +31,18 @@ func main() {
 	profit = filereader.ReadWeightAndProfit("testdata/profit")
 
 	// Create a map with key and values
-	items := []Item {}
+	items := []knapsack.Item {}
 	for i := 0; i < len(weight); i++ {
 		key := "item" + strconv.Itoa(i)
-		items = append(items, Item {key, weight[i], profit[i]})
+		items = append(items, knapsack.Item {key, weight[i], profit[i]})
 	}
+
+	fmt.Printf("Capacity: %[1]d \n", capacity)
 	fmt.Println("Items: ", items)
 
-	knapsack(items, capacity)
+	knapsack.SolveRecursive(items, capacity)
 
 }
 
 
-func knapsack(items []Item, knapsackSize int) Solution {
-	return Solution {nil, 0, 0};
-}
+
---
+++ src/knapsack/knapsack.go
@@ -0,0 +1,48 @@
+package knapsack
+
+import "fmt"
+
+type Item struct {
+	Name   string
+	Weight int
+	Value  int
+}
+
+type Solution struct {
+	Items       *[]Item // we just need the reference to save memory
+	TotalValue  int
+	TotalWeight int
+}
+
+func SolveRecursive(items []Item, knapsackSize int) Solution {
+	result, w, v := m(items, len(items)-1, knapsackSize)
+	fmt.Printf("\n")
+	fmt.Printf("################################# RESULT ##################################\n")
+	fmt.Println("Take the following items: ",result)
+	fmt.Println("weight:", w)
+	fmt.Println("value:", v)
+	fmt.Printf("###########################################################################\n")
+	//generate(items)
+	return Solution {nil, 0, 0};
+}
+
+func generate(items []Item) {
+	//var partOne = items[:len(items)/2]
+	//var partTwo = items[len(items)/2:]
+
+}
+
+func m(items []Item, itemSize int, maxWeight int) ([]string, int, int) {
+	if itemSize < 0 || maxWeight == 0 {
+		return nil, 0, 0
+	} else if items[itemSize].Weight > maxWeight {
+		return m(items,itemSize-1, maxWeight)
+	}
+	i0, w0, v0 := m(items,itemSize-1, maxWeight)
+	i1, w1, v1 := m(items, itemSize-1, maxWeight-items[itemSize].Weight)
+	v1 += items[itemSize].Value
+	if v1 > v0 {
+		return append(i1, items[itemSize].Name), w1 + items[itemSize].Weight, v1
+	}
+	return i0, w0, v0
+}
